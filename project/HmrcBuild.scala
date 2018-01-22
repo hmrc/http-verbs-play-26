2a9550b5bb6eab26f3928cde0955332cd9964b4e
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2018-01-22 13:36:16 +0000

    Initial commit

---
+++ project/HmrcBuild.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2015 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import sbt.Keys._
+import sbt._
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object HmrcBuild extends Build {
+
+  import uk.gov.hmrc._
+
+  val appName = "http-verbs-play-26"
+
+  lazy val microservice = Project(appName, file("."))
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
+    .settings(
+      scalaVersion := "2.11.7",
+      libraryDependencies ++= AppDependencies(),
+      crossScalaVersions := Seq("2.11.7"),
+      resolvers := Seq(
+        Resolver.bintrayRepo("hmrc", "releases"),
+        "typesafe-releases" at "http://repo.typesafe.com/typesafe/releases/"
+      ),
+      version := "100.0-SNAPSHOT"
+    )
+}
+
+private object AppDependencies {
+
+  import play.sbt.PlayImport._
+  import play.core.PlayVersion
+
+//  private val playVersion = "2.5.15"
+
+  val compile = Seq(
+    "com.typesafe.play" %% "play" % PlayVersion.current,
+    ws,
+    "uk.gov.hmrc" %% "time" % "2.0.0",
+    "uk.gov.hmrc" %% "http-core" % "0.5.0"
+  )
+
+
+  trait TestDependencies {
+    lazy val scope: String = "test"
+    lazy val test: Seq[ModuleID] = ???
+  }
+
+
+
+  object Test {
+    def apply() = new TestDependencies {
+      override lazy val test = Seq(
+        "com.typesafe.play" % "play-test_2.11" %  PlayVersion.current % scope,
+        "com.typesafe.play" % "play-specs2_2.11" %  PlayVersion.current % scope,
+        "commons-codec" % "commons-codec" % "1.7" % scope,
+        "org.scalatest" %% "scalatest" % "2.2.4" % scope,
+        "org.scalacheck" %% "scalacheck" % "1.12.2" % scope,
+        "org.pegdown" % "pegdown" % "1.5.0" % scope,
+        "com.github.tomakehurst" % "wiremock" % "1.52" % scope,
+        "uk.gov.hmrc" %% "http-verbs-test" % "1.1.0" % scope,
+        "ch.qos.logback" % "logback-core" % "1.1.7",
+        "ch.qos.logback" % "logback-classic" % "1.1.7"
+      )
+    }.test
+  }
+
+  def apply() = compile ++ Test()
+}
---
+++ src/test/scala/uk/gov/hmrc/play/Examples.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+///!@*
+// * Copyright 2017 HM Revenue & Customs
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//package uk.gov.hmrc.play
+//
+//object Examples {
+//
+//  import uk.gov.hmrc.play.http._
+//  import uk.gov.hmrc.play.http.ws._
+//
+//  trait ConnectorWithHttpValues {
+//    val http: HttpGet with HttpPost
+//  }
+//  object ConnectorWithHttpValues extends ConnectorWithHttpValues {
+//    val http = new WSGet with WSPost {
+//      val hooks = NoneRequired
+//    }
+//  }
+//
+//
+//  trait ConnectorWithMixins extends HttpGet with HttpPost {
+//  }
+//  object ConnectorWithMixins extends ConnectorWithMixins with WSGet with WSPost {
+//    val hooks = NoneRequired
+//  }
+//
+//  trait VerbExamples {
+//    val http: HttpGet with HttpPost with HttpPut with HttpDelete with HttpPatch
+//    implicit val hc = HeaderCarrier()
+//
+//    import play.api.libs.json._
+//    case class MyCaseClass(a: String, b: Int)
+//    implicit val f = Json.reads[MyCaseClass]
+//    http.GET[MyCaseClass]("http://gov.uk/hmrc") // Returns an MyCaseClass de-serialised from JSON
+//
+//    import play.twirl.api.Html
+//    http.GET[Html]("http://gov.uk/hmrc") // Returns a Play Html type
+//
+//    http.GET[Option[MyCaseClass]]("http://gov.uk/hmrc") // Returns None, or Some[MyCaseClass] de-serialised from JSON
+//    http.GET[Option[Html]]("http://gov.uk/hmrc") // Returns a None, or a Play Html type
+//  }
+//}
