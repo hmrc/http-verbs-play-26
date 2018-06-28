10d0b9c1398ac8d2ef052c133a58c722cd14ad44
Konrad Wudkowski <wudkowski@gmail.com>
2018-06-28 15:12:09 +0100

    PLATOPS-1387: Upgrade http-core to latest version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile = Seq(
     "com.typesafe.play" %% "play"      % PlayVersion.current,
-    "uk.gov.hmrc"       %% "http-core" % "0.9.0",
+    "uk.gov.hmrc"       %% "http-core" % "0.10.0",
     ws
   )
 
