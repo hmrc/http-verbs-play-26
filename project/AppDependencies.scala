0026e54ad0df57fa9c2cd521a9bc5294e8c20021
José Miguel <474643+jm2dev@users.noreply.github.com>
2019-02-14 17:09:12 +0000

    PLATOPS-1930 deps upgrade.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,21 +5,21 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "com.typesafe.play" %% "play"      % PlayVersion.current,
-    "uk.gov.hmrc"       %% "http-core" % "1.1.0",
+    "com.typesafe.play" %% "play"      % "2.6.21",
+    "uk.gov.hmrc"       %% "http-core" % "1.3.0",
     ws
   )
 
   val test = Seq(
-    "com.typesafe.play"      %% "play-test"      % PlayVersion.current % Test,
-    "com.typesafe.play"      %% "play-specs2"    % PlayVersion.current % Test,
-    "commons-codec"          % "commons-codec"   % "1.11"              % Test,
+    "com.typesafe.play"      %% "play-test"      % "2.6.21" % Test,
+    "com.typesafe.play"      %% "play-specs2"    % "2.6.21" % Test,
+    "commons-codec"          % "commons-codec"   % "1.12"              % Test,
     "org.scalatest"          %% "scalatest"      % "3.0.5"             % Test,
     "org.scalacheck"         %% "scalacheck"     % "1.14.0"            % Test,
-    "org.pegdown"            % "pegdown"         % "1.5.0"             % Test,
-    "com.github.tomakehurst" % "wiremock"        % "1.52"              % Test,
-    "ch.qos.logback"         % "logback-core"    % "1.1.7"             % Test,
-    "ch.qos.logback"         % "logback-classic" % "1.1.7"             % Test,
+    "org.pegdown"            % "pegdown"         % "1.6.0"             % Test,
+    "com.github.tomakehurst" % "wiremock"        % "1.58"              % Test,
+    "ch.qos.logback"         % "logback-core"    % "1.1.11"             % Test,
+    "ch.qos.logback"         % "logback-classic" % "1.1.11"             % Test,
     "org.webbitserver"       % "webbit"          % "0.4.15"            % Test
   )
 
